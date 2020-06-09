98c2db9fbb084033940404b7ed993401d95c5ab2
Eduardo Arruda <caduknebel@gmail.com>
2020-06-09 17:18:54 -0300

    feat: adiciona funcionalidade de listagem, criação e deleção de pets

---
+++ app/Http/Controllers/API/PetController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Pet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Pet $pet)
+    {
+        return $pet->all()->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Pet $pet)
+    {
+        $request->validate([
+            'name' => 'required|min:2',
+            'specie' => ['required', 
+                Rule::in(['C', 'G'])    
+            ],
+        ]);
+
+        $pet->name = $request->name;
+        $pet->specie = $request->specie;
+        $pet->save();
+
+        return response('success', 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pet $pet)
+    {
+        return $pet->toJson();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pet $pet)
+    {
+        $pet->delete();
+
+        return response('deleted', 200);
+    }
+}
